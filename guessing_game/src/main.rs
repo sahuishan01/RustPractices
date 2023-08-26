0e0429c2fcb9fe8f7d932f2405781642e69c952b
Ishan Sahu <ishan20015.s@gmail.com>
2023-08-27 01:14:17 +0530

    guessing game added

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,25 @@
+use std::io;  // for getting the input from user
+use rand::Rng; // for generating random number
+use std::cmp::Ordering;
+fn main() {
+    println!("Welcome to Guess The Number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop{
+        println!("Enter a number that you want to guess");
+        let mut guess = String::new(); // initializing an empty string for storing user input
+        io::stdin().read_line(&mut guess).expect("Failed to read line"); //reading user input as string, and throwing any error if it occurs while getting input
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue
+            }
+        };
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {println!("You win!"); break;},
+        }
+    }
+
+}
